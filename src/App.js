5b84e0eceb9fdc1b763700a1a92ec74320d09877
jsanchez78 <jsanch75@uic.edu>
2020-06-05 14:52:59 -0500

    Center svg icons + links to accounts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import twitter from './icons/twitter.svg';
+import github from './icons/github.svg';
+import linkedin from './icons/linkedin.svg';
 import './App.css';
 
 function App() {
@@ -22,7 +24,26 @@ function App() {
         </a>
       </header>
       <div className="icons">
-      <img src={twitter} className="Twitter-logo" alt="logo"/>
+        <div className="col">
+          <a href="https://twitter.com/JacobSa50291880">
+            <img src={twitter} className="Twitter-logo" alt="logo"/>
+          </a>
+        </div>
+        <div className="col">
+          <a href="https://github.com/jsanchez78">
+            <img src={github} className="Twitter-logo" alt="logo"/>
+          </a>
+        </div>
+        <div className="col">
+          <a href="https://www.linkedin.com/in/jacob-sanchez-12b751142/">
+            <img src={twitter} className="Twitter-logo" alt="logo"/>
+          </a>
+        </div>
+        <div className="col">
+          <a href="https://dev.to/jsanchez78">
+            <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="jsanchez78's DEV Profile" height="30" width="30"/>
+          </a>
+        </div>
       </div>
     </div>
   );
